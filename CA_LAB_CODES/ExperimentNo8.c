4bcf7a26661ccb0e1d1139bea21b2f52cd0ba566
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-01-01 03:52:31 +0530

    Added Code

--- CA_LAB_CODES/ExperimentNo8.c
+++ CA_LAB_CODES/ExperimentNo8.c
@@ -11,6 +11,7 @@ void floyds(int p[10][10],int n)
     else
         p[i][j]=min(p[i][j],p[i][k]+p[k][j]);
 }
+
 int min(int a,int b)
 {
     if(a<b)
@@ -18,6 +19,7 @@ int min(int a,int b)
     else
         return(b);
 }
+
 void main()
 {
     int p[10][10],w,n,e,u,v,i,j;;
@@ -30,12 +32,14 @@ void main()
         for(j=1;j<=n;j++)
         p[i][j]=999;
     }
+    
     for(i=1;i<=e;i++)
     {
         printf("\n Enter the end vertices of edge%d with its weight \n",i);
         scanf("%d%d%d",&u,&v,&w);
         p[u][v]=w;
     }
+
     printf("\n Matrix of input data:\n");
     for(i=1;i<=n;i++)
     {
@@ -43,15 +47,18 @@ void main()
         printf("%d \t",p[i][j]);
         printf("\n");
     }
+
     floyds(p,n);
     printf("\n Transitive closure:\n");
     for(i=1;i<=n;i++)
+
     {
     for(j=1;j<=n;j++)
     printf("%d \t",p[i][j]);
     printf("\n");
     }
     printf("\n The shortest paths are:\n");
+
     for(i=1;i<=n;i++)
     for(j=1;j<=n;j++)
     {
