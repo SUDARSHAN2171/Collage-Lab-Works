8c538ba61efca1fa59e7afa15a3a4d9c829186f2
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-02-12 15:22:21 +0530

    Added Code

--- ML_LAB_CODES/Learning Basics of Python/Datatypes.py
+++ ML_LAB_CODES/Learning Basics of Python/Datatypes.py
@@ -3,7 +3,6 @@ age = 18           #this is a integer data  type
 price = 20.4       #this is a float data type
 value = True       #this is bool data type
 Job = None         #this is None data type
-
 print(type(name))
 print(type(age))
 print(type(price))
