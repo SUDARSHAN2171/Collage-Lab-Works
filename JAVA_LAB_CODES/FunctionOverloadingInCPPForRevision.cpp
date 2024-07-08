b2b7860bb41e3b313dfbc72d6941436c63bf88cd
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-08 11:01:39 +0530

    Added Codes

---
+++ JAVA_LAB_CODES/FunctionOverloadingInCPPForRevision.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+class shapes{
+    public:
+    float radius,length,breadth;
+    int side;
+    int area();
+};
+int area(int side)
+    {
+        return side*side;
+    }
+
+float area(float radius)
+{
+    return 3.14*radius*radius;
+}
+
+float area(float length,float breadth)
+{
+    return length*breadth;
+}
+int main()
+{
+    shapes s;
+    char choose;
+    cout<<"Enter the shape (C/S/R): ";
+    cin>>choose;
+    switch (choose)
+    {
+    case 'C':
+        {
+            cout<<"Enter the radius: ";
+            cin>>s.radius;
+            cout<<"Area of circle: "<<area(s.radius)<<endl;
+            break;
+        }
+        
+    case 'S':
+        {
+            cout<<"Enter the side: ";
+            cin>>s.side;
+            cout<<"Area of square: "<<area(s.side)<<endl;
+            break;
+        }
+        
+    case 'R':
+        {
+            cout<<"Enter the length and breadth: ";
+            cin>>s.length>>s.breadth;
+            cout<<"Area of rectangle: "<<area(s.length,s.breadth)<<endl;
+            break;
+        }
+    
+    default:
+        break;
+    }
+    
+}
---
+++ JAVA_LAB_CODES/SingleInheritanceInCPPForRevision.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+class student
+{
+    int rollno;
+    char div;
+    public :
+
+    void setdate();
+    int getinfo1();
+    char getinfo2();
+};
+void student ::  setdate(void)
+{
+    int a;
+    char b;
+    cout<<"Enter Roll Number : ";
+    cin>>a;
+    cout<<"Enter Division : ";
+    cin>>b;
+    rollno = a;
+    div = b;
+}
+ int student :: getinfo1()
+{
+     return rollno;
+ }
+
+char student :: getinfo2()
+{
+     return div;   
+}
+class marks : public student
+{   
+    public :
+    int m1,m2,m3;
+    int inputMarks();
+    void display();
+};
+int marks :: inputMarks()
+{
+    cout<<"Enter Marks in Subject 1 : ";
+    cin>>m1;
+    cout<<"Enter Marks in Subject 2 : ";
+    cin>>m2;
+    cout<<"Enter Marks in Subject 3 : ";
+    cin>>m3;
+}
+
+void marks :: display()
+{
+    cout<<endl<<"Roll No : "<<getinfo1()<<endl<<"Division : "<<getinfo2()<<endl;
+    cout<<"Subject 1 : "<<m1<<endl;
+    cout<<"Subject 2 : " <<m2<<endl;
+    cout<<"Subject 3 : "<<m3<<endl;  
+
+}
+int main()
+{
+    marks d;
+    d.setdate();
+    d.inputMarks();
+    d.display();
+}
