c344e4c9c5db8e758eb99cf183328567ac6e98f7
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-10 23:21:27 +0530

    Added Code

--- ML_LAB_CODES/Learning Python/List.py
+++ ML_LAB_CODES/Learning Python/List.py
@@ -13,3 +13,18 @@ print(marks)
 print(marks[1:3])
 print(marks[-4:-1])
 print(marks[-5:len(marks)])
+print(marks[:4])
+
+#methods in list
+number=[2,1,3]
+print(number)
+number.append(4)
+print(number)
+number.sort()
+print(number)
+number.sort(reverse=True)
+print(number)
+number.reverse()
+print(number)
+number.insert(2,7)
+print(number)
---
+++ ML_LAB_CODES/Learning Python/Tuple.py
@@ -0,0 +1,18 @@
+#a built-in data type that lets us create immutable sequences of values
+tup=(87,64,33,95,76)
+print(type(tup))
+#tup[0]=43 this is not allowed
+tup1=(1)
+print(type(tup1)) #this is not the correct way to waite tuple
+tup2=(1,)
+print(type(tup2)) #this is the correct way to write tuple
+
+tup3=tup*5   #this is used to print n element n times in tuple
+print(tup3)
+
+#scaling in tuple
+print(tup[3:len(tup)])
+
+#methods in tuple
+print(tup.index(95)) #returns the first index of the value
+print(tup.count(95)) #returns the count of the value in tuple
