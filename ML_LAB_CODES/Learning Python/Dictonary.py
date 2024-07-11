068712f3e3e5938f6d7b8259c446ec1fd1bafeb3
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-11 22:17:38 +0530

    Added Code

--- ML_LAB_CODES/Learning Python/Dictonary.py
+++ ML_LAB_CODES/Learning Python/Dictonary.py
@@ -15,3 +15,24 @@ dict["surname"]="patil"
 print(dict["name"])
 print(dict["surname"])
 
+student={
+    "name":"Sudarshan Jadhav",
+    "PRN":"22UAI039",
+    "Subject":{
+        "Math":97,
+        "Science":98,
+        "English":95
+    }
+}
+print(student)
+#methods in dictonary
+print(len(list(student.keys())))
+print(student.keys())   #return all the keys
+print(student.values()) #return all values
+print(student.items())  #return all (key,value) pair in tuple
+#print(student["name2"]) this will give error as the is no such name2 but the below wont give an error just it will return None
+print(student.get("name2")) 
+print(student.get("name")) #return the key according to value
+new_student={"City":"Sangli","name":"Ujal"} # name will override over sudarshan as ujal
+print(student.update(new_student))    #inserts the specified items to the dictionary
+print(student)
---
+++ ML_LAB_CODES/Learning Python/Set.py
@@ -0,0 +1,5 @@
+#set is the collection of the unordered items
+#each element in the set must be unique & immutable
+
+set1={1,2,3,4,5,5}
+print(set1)
