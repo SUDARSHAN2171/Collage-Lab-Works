a62742b2f1b3e9cb9212456377a92b2feb0c5755
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-12 09:55:54 +0530

    Added Code

--- ML_LAB_CODES/Learning Python/Set.py
+++ ML_LAB_CODES/Learning Python/Set.py
@@ -3,3 +3,8 @@
 
 set1={1,2,3,4,5,5}
 print(set1)
+print(type(set1))
+set2={1,1,1,2,3,3,7,9,2,"Home","World"}
+print(set2)
+#set when printed always return same answer in different order and will not repeat the number or world which you used more then one time 
+print(len(set2)) #even len will ignore dublicates
