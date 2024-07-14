bcf84b063a39d544b0e67aa0e4010ec2879f508e
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-14 20:28:02 +0530

    Added Code

--- ML_LAB_CODES/Learning Python/Loops.py
+++ ML_LAB_CODES/Learning Python/Loops.py
@@ -48,3 +48,60 @@ for val in tup:
 anem="Sudarshan"
 for char in anem:
     print(char)
+#linear search
+l=[1,4,9,16,25,36,49,64,81,100]
+x=25
+index=0
+for i in l:
+    print(i)
+    if(x==i):
+        print("Element found at ", index)
+        break
+    index+=1
+    
+#range
+#range function return a numbers, starting from 0by default, and increments by 1(by default),and stops before a specificed number
+#syntax (Start?,stop,step?)
+print(range(5)) 
+seq = range(10)
+for i in seq: #this is stop condition
+    print(i)
+#or    
+for i in range(10): 
+    print(i)
+    
+for i in range(2,10): #this is start and stop condition 
+    print(i)
+    
+for i in range(2,10,2): #here the last 2 is the number of increment in loop
+    print(i) 
+    
+#pass statement
+#pass is a null statement that does nothing. It is used as a placeholder for future code
+#syntax
+# for el in range(10):
+#     pass
+
+# for i in range(5):
+#     #assume this loop is empty for some reason
+# print("Something")
+#the above will give error so there is an alternative
+
+for i in range(5):
+    pass
+print("Something")
+
+#wap to calculate sum of first five numbers
+sum=0
+for i in  range(1,6):
+    sum=sum+i
+print(sum)
+
+#wap to find factorial of first n numbers
+give=int(input("Enter a number to find its Factorial : "))
+fact=1
+anyno=1
+while anyno<=give:
+    fact*=anyno
+    anyno+=1
+print("Factorial of",give," : ",fact)
