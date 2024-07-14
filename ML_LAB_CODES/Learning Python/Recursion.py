46886b2e46acb2f6bfe78fe3410d36b2a6b5a9a8
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-14 23:54:51 +0530

    Added Code

--- ML_LAB_CODES/Learning Python/Recursion.py
+++ ML_LAB_CODES/Learning Python/Recursion.py
@@ -1,7 +1,7 @@
 #when a function call itself repeatedly
 def show(n):
-    if(n==0):
+    if(n==0):  #base case it is where the loop will end
         return
     print(n)
     show(n-1)
-show(5)
+show(5) 
