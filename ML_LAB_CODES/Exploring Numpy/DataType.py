293c2fb4fa48fc98c3810cc4b7b1e00ce4784bc0
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-16 23:37:19 +0530

    Added Code

---
+++ ML_LAB_CODES/Exploring Numpy/DataType.py
@@ -0,0 +1,22 @@
+import numpy as np
+'''
+Data Types in NumPy
+NumPy has some extra data types, and refer to data types with one character, like i for integers, u for unsigned integers etc.
+
+Below is a list of all data types in NumPy and the characters used to represent them.
+
+i - integer
+b - boolean
+u - unsigned integer
+f - float
+c - complex float
+m - timedelta
+M - datetime
+O - object
+S - string
+U - unicode string
+V - fixed chunk of memory for other type ( void )
+'''
+#Checking the Data Type of an Array
+arr = np.array([1, 2, 3, 4])
+print(arr.dtype)
--- ML_LAB_CODES/Exploring Numpy/SlicingArray.py
+++ ML_LAB_CODES/Exploring Numpy/SlicingArray.py
@@ -9,3 +9,26 @@ print("This will automaticly print the array from index 4 to the last : ",array1
 print("This will print the array from begin to the 4th index : ",array1[:4])
 
 #Negative Slicing
+array4=np.array([1,2,3,4,5])
+print("This is a example of negative slicing : ",array4[-3:-1])
+
+
+#step
+#Use the step value to determine the step of the slicing:
+array5 = np.array([1, 2, 3, 4, 5, 6, 7])
+print("This is a example of negative slicing with incremanation : ",array5[1:5:2])        #the 2 in the block represents the increment while running this
+
+array6 = np.array([1, 2, 3, 4, 5, 6, 7])
+print("This will print all the old numbers : ",array6[::2])      #if there is no begin of end set but the step is set then
+
+#Slicing 2-D arrays
+array7 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print("This will print the value at array7[1, 1:4] : ",array7[1, 1:4])
+
+#From both elements, return index 2:
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print("This will print the value at arr[0:2, 2] :",arr[0:2, 2])
+
+#From both elements, slice index 1 to index 4 (not included), this will return a 2-D array
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print("This will print the value at arr[0:2, 1:4] : ",arr[0:2, 1:4])
