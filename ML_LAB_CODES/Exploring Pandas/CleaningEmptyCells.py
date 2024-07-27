f1a441e4f5b677846b83e20e3b5713c0e2a6d1aa
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-27 21:05:42 +0530

    Added Code

--- ML_LAB_CODES/Exploring Pandas/CleaningEmptyCells.py
+++ ML_LAB_CODES/Exploring Pandas/CleaningEmptyCells.py
@@ -8,5 +8,11 @@ one way to deal with empty cells is to remove rows that contain empty cells
 This is usually OK,since the data sets can be very big, and removing a few rows will not have a big impact on the result
 '''
 data=pd.read_csv('data1.csv')
-new_data=data.dropna()
+new_data=data.dropna()      #this will not change the original dataframe
 print(new_data.to_string())
+print("\n")
+
+#if we want to change the original dataframe use the 'inplace=True' argument:
+data.dropna(inplace=True)
+print(data.to_string())
+#Now, the dropna(inplace = True) will NOT return a new DataFrame, but it will remove all rows containing NULL values from the original DataFrame.
