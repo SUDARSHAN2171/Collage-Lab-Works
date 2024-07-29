2af72953dea3a844abafe7023ca256facd854f35
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-29 20:40:49 +0530

    Added Code

---
+++ ML_LAB_CODES/Exploring Pandas/CleaningWrongFormat.py
@@ -0,0 +1,3 @@
+#Data of Wrong Format
+#Cells with data of wrong format can make it difficult, or even impossible, to analyze data.
+#To fix it, you have two options: remove the rows, or convert all cells in the columns into the same format.
