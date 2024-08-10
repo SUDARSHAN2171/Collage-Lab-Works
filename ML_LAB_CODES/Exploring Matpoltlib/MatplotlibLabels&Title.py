0eaef4f13ae91978d7111527b01256411c4e7a93
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-08-10 21:14:45 +0530

    Added Code

--- ML_LAB_CODES/Exploring Matpoltlib/MatplotlibLabels&Title.py
+++ ML_LAB_CODES/Exploring Matpoltlib/MatplotlibLabels&Title.py
@@ -43,3 +43,10 @@ plt.show()
 you can use the loc parameter in title() to position the tile
 legal value are : 'left','right' and 'center'. Default value is 'center'
 '''
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+plt.title("Sudarshan's Data", loc = 'left')
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burned")
+plt.plot(x, y)
+plt.show()
