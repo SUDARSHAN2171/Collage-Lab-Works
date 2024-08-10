1d3a6d40cae52c4e3749afaf6bf75b093dae7fd5
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-08-10 21:42:52 +0530

    Added Code

--- ML_LAB_CODES/Exploring Matpoltlib/MatplotlibAddingGridLines.py
+++ ML_LAB_CODES/Exploring Matpoltlib/MatplotlibAddingGridLines.py
@@ -21,9 +21,10 @@ Legal values are: 'x', 'y', and 'both'. Default value is 'both'.
 '''
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
-plt.title("Sports Watch Data")
+plt.title("Sudarshan's Data")
 plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
+plt.ylabel("Calorie Burned")
 plt.plot(x, y)
 plt.grid(axis = 'x')
 plt.show()
+
