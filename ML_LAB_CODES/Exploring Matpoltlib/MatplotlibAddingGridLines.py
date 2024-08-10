24716e27b5048957b6ad50c9a7c8849db1313c65
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-08-10 21:31:22 +0530

    Added Code

---
+++ ML_LAB_CODES/Exploring Matpoltlib/MatplotlibAddingGridLines.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#add gird lines to a plot 
+'''
+with pyplot you can use the grid() function to add grid lines to the plot
+'''
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+plt.title("Sudarshan's Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burned")
+plt.plot(x, y)
+plt.grid()
+plt.show()
+
+#specify which grid lines to display
+'''
+You can use the axis parameter in the grid() function to specify which grid lines to display.
+Legal values are: 'x', 'y', and 'both'. Default value is 'both'.
+'''
