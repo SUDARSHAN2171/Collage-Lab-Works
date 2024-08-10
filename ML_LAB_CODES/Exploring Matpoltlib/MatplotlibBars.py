af3b944b07c4575af4e9a7d981cdac38e17266bc
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-08-10 23:12:09 +0530

    Added Code

---
+++ ML_LAB_CODES/Exploring Matpoltlib/MatplotlibBars.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Creating Bars
+'''
+With Pyplot, you can use the bar() function to draw bar graphs:
+'''
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.bar(x,y)
+plt.show()
+
+#Horizontal Bars
+'''
+If you want the bars to be displayed horizontally instead of vertically, use the barh() function:
+'''
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.barh(x, y)
+plt.show()
+
+#Bar Color
+'''
+The bar() and barh() take the keyword argument color to set the color of the bars:
+'''
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+plt.bar(x, y, color = "red")
+plt.show()
