573fb45acb6fbbb9522e58851cad2a6951411e55
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-08-10 21:40:35 +0530

    Added Code

--- ML_LAB_CODES/Exploring Matpoltlib/MatplotlibAddingGridLines.py
+++ ML_LAB_CODES/Exploring Matpoltlib/MatplotlibAddingGridLines.py
@@ -19,3 +19,11 @@ plt.show()
 You can use the axis parameter in the grid() function to specify which grid lines to display.
 Legal values are: 'x', 'y', and 'both'. Default value is 'both'.
 '''
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+plt.plot(x, y)
+plt.grid(axis = 'x')
+plt.show()
