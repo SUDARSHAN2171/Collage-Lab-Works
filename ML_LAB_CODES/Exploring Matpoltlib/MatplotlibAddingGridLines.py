03dc8c35ae47f591b0bc8a63aefe6383d3d1e24b
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-09-09 10:32:47 +0530

    Added Code

--- ML_LAB_CODES/Exploring Matpoltlib/MatplotlibAddingGridLines.py
+++ ML_LAB_CODES/Exploring Matpoltlib/MatplotlibAddingGridLines.py
@@ -32,6 +32,7 @@ plt.show()
 '''
 you can also set the line properties of the grid, like this: grid (color='color',linestyle = 'linestyle',linewidth=number)
 '''
+
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 plt.title("Sports Watch Data")
