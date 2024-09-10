bc7e70742eaf372847a84a8cb278662471f716de
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-09-10 12:45:29 +0530

    Added Code

--- ML_LAB_CODES/Exploring Matpoltlib/MatplotlibBars.py
+++ ML_LAB_CODES/Exploring Matpoltlib/MatplotlibBars.py
@@ -9,7 +9,6 @@ x = np.array(["A", "B", "C", "D"])
 y = np.array([3, 8, 1, 10])
 plt.bar(x,y)
 plt.show()
-
 #Horizontal Bars
 '''
 If you want the bars to be displayed horizontally instead of vertically, use the barh() function:
@@ -35,7 +34,6 @@ x = np.array(["A", "B", "C", "D"])
 y = np.array([3, 8, 1, 10])
 plt.bar(x, y, color = "hotpink")
 plt.show()
-
 #color Hex
 x = np.array(["A", "B", "C", "D"])
 y = np.array([3, 8, 1, 10])
