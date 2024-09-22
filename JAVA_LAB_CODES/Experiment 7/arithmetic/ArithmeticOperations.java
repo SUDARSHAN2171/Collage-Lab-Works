177db35b1e1c1aa08b091a15a73aacb84f853c0f
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-09-23 04:55:55 +0530

    Added Code

---
+++ JAVA_LAB_CODES/Experiment 7/arithmetic/ArithmeticOperations.java
@@ -0,0 +1,23 @@
+package arithmetic;
+
+public class ArithmeticOperations {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+}
