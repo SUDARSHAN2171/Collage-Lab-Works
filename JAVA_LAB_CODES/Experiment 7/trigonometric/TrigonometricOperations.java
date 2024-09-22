e5d2e5061ec4231882b31d09ceefc32f54ab9044
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-09-23 04:56:22 +0530

    Added Code

---
+++ JAVA_LAB_CODES/Experiment 7/trigonometric/TrigonometricOperations.java
@@ -0,0 +1,16 @@
+package trigonometric;
+
+public class TrigonometricOperations {
+
+    public double sin(double angle) {
+        return Math.sin(Math.toRadians(angle));
+    }
+
+    public double cos(double angle) {
+        return Math.cos(Math.toRadians(angle));
+    }
+
+    public double tan(double angle) {
+        return Math.tan(Math.toRadians(angle));
+    }
+}
